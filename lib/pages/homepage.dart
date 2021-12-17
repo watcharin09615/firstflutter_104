65ec5ef4698b21c3873bc859071236e16ee4668f
watcharin <filmm09615@gmail.com>
2021-12-17 11:50:36 +0700

    171264

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({ Key? key }) : super(key: key);
+
+  get child => null;
 
   @override
   Widget build(BuildContext context) {
@@ -12,9 +15,40 @@ class MyHomePage extends StatelessWidget {
       ),
       body: ListView(
         children :[
+          profile(),
           firstname(),
-
+          birthday(),
+          telephone(),
+          
         ] ,
+      ),
+    );
+  }
+
+  CircleAvatar profile() => const CircleAvatar(
+                radius: 150.0,
+                backgroundImage:
+                    NetworkImage('https://scontent.fhdy2-1.fna.fbcdn.net/v/t39.30808-6/241438482_994640744653310_6682229638160222983_n.jpg?_nc_cat=110&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeHYE7q4-vHTJHywLFc8LOFIglEZ0xxOvIOCURnTHE68g36HW1ip9ZqZf6R4uGpjB9_Gytj87F6g5K2ky2WloeD7&_nc_ohc=yNnY_WyPBFQAX8JIQ7c&_nc_zt=23&_nc_ht=scontent.fhdy2-1.fna&oh=00_AT-_tTFDS4v6VAYxPaM2qYT7m66T2UAVYwH_-3aiE64T5A&oe=61C147BC'),
+                backgroundColor: Colors.transparent,
+              );
+
+  Card birthday() {
+    return Card(
+      color: Colors.pink[200],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children:[
+          const FaIcon(FontAwesomeIcons.birthdayCake),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Text(
+              "18 กุมภาพันธ์ 2564",
+              style: GoogleFonts.mali(
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -24,9 +58,26 @@ class MyHomePage extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Padding(
-          padding: const EdgeInsets.all(20.0),
+          padding: const EdgeInsets.all(10.0),
           child: Text(
             "นายวัชรินทร์ โรจเรณุมาศ",
+            style: GoogleFonts.mali(
+              color: Colors.black,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+  Row telephone() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const FaIcon(FontAwesomeIcons.phoneSquareAlt),
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Text(
+            "062-2266723",
             style: GoogleFonts.mali(
               color: Colors.black,
             ),
